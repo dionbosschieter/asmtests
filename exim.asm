43ac644a39751a3aae183bd7faa5d10a06297f5e
Dion Bosschieter <dbosschieter@transip.nl>
2023-10-07 13:32:47 +0200

    Add parse ip from string functionality to exim.asm

--- exim.asm
+++ exim.asm
@@ -12,9 +12,11 @@ section .data
     addr istruc sockaddr
         at sin_family ,  dw 2
         at sin_port ,    db 0x00, 0x19
-        at sin_addr ,    db 37, 97, 128, 3 ; 37.97.128.3
+        at sin_addr ,    db 0, 0, 0, 0
         at sin_padding , db 0,0,0,0,0,0,0,0
     iend
+
+    ipstring:  db "37.97.128.3", 0xa, 0xd,
 
 section .bss
     response resd 1
@@ -23,6 +25,8 @@ section .text
 global _start
 
 _start:
+
+
 
 _socket:
     mov rbx, 2 ; AF_INET
@@ -34,16 +38,65 @@ _socket:
     jz  _exit
     mov [fd], rax ; save file descriptor
 
-setsockopt:
-    mov rax, 366
-    mov rbx,[fd]
-    mov rcx, 0x0001 ; sol_socket
-    mov rdx, 0x0002 ; reuseaddr
-    mov rsi, one ; true
-    mov rdi, 4 ; true
-    int 0x80 ; syscall
-    cmp rax,-1
-    jz  _exit
+_startreadip:
+    mov rcx, ipstring ; point rax to ipstring buffer
+    xor rdx, rdx      ; zero output char
+    mov r8, 0         ; count for every pair, we start at 1
+
+_readip:
+    mov bl, [rcx]  ; load char from buffer into rbx, we use bl here
+                   ; because we don't want to load more than the current
+                   ; 8 bits of the char
+    cmp rbx, '.'   ; if this is a '.' we reached the end of the integer
+    jz _ereadip    ; then we are finished for this cycle
+    cmp rbx, 0xa   ; we are done, end of string
+    jz _ereadip    ; we are finished
+
+    sub rbx, '0'   ; convert from ascii to dec
+
+    cmp rdx, 0     ; we check if we already stored a number in rdx
+    jz _currzero   ; we don't have to do any multiplication
+
+    mov rax, 10    ; multiply current number by 10
+    mul rdx        ; multiply by 10
+    mov rdx, rax   ; store result in rdx again
+    add rdx, rbx   ; add (+) the new number to the multiplied number
+    jmp _nextchar
+
+_ereadip:
+    inc r8
+    cmp r8, 4
+    cmovz r12, rdx
+    jge _storip
+    cmp r8, 3
+    cmovz r11, rdx
+    cmp r8, 2
+    cmovz r10, rdx
+    cmp r8, 1
+    cmovz r9, rdx
+
+    ; start next cycle of ascii -> dec
+    xor rdx, rdx  ; reset output register rdx
+    jmp _nextchar ; start with next cycle
+
+_currzero:
+    mov rdx, rbx
+
+_nextchar:
+    inc rcx        ; increment to next char
+    jmp _readip    ; read next char
+
+_storip:
+    ; we start with the last digit, because it is in reverse order
+    mov rax, r12
+    shl rax, 8
+    or rax, r11
+    shl rax, 8
+    or rax, r10
+    shl rax, 8
+    or rax, r9
+
+    mov [addr + sin_addr], rax ; stor ip in struct
 
 _connect:
     mov rax, 362
